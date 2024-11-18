33f952ae2d7cfa662c4984d398126021679a79d8
R. Ilham Sastronegoro <7355835+radenpioneer@users.noreply.github.com>
2024-11-18 15:39:36 +0000

    setup web docker image

---
+++ web/server.mjs
@@ -0,0 +1,18 @@
+// @ts-check
+import express from 'express'
+import morgan from 'morgan'
+
+// @ts-ignore
+import { handler as ssrHandler } from './dist/server/entry.mjs'
+
+const app = express()
+app.use('/', express.static('dist/client'))
+app.use(ssrHandler)
+app.use(morgan('combined'))
+
+const PORT = process.env.PORT || 4321
+const HOST = process.env.HOST || '0.0.0.0'
+
+app.listen(PORT, HOST, () => {
+    `Server started at ${HOST}:${PORT}`
+})
