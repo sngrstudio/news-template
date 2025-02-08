8bb40300f253a3528fedc32eea743ee64b8535b8
R. Ilham Sastronegoro <7355835+radenpioneer@users.noreply.github.com>
2025-02-08 17:58:51 +0700

    Initial Content Backend Development (#2)
    * Setup local development environment
    * Setup (Bedrock) WordPress image
    * Setup and test Kamal deployment

---
+++ content/web/app/themes/headless/functions.php
@@ -0,0 +1,23 @@
+<?php
+  function register_my_menus() {
+    register_nav_menus(
+    array(
+      'primary-navigation' => __( 'Primary Navigation' ),
+      'sidebar-navigation' => __( 'Sidebar Navigation' ),
+      'footer-links' => __( 'Footer Links' ),
+      'social' => __( 'Social' )
+    )
+    );
+  }
+  add_action( 'init', 'register_my_menus' );
+  add_theme_support('post-thumbnails');
+
+  include_once( ABSPATH . 'wp-admin/includes/plugin.php' );
+  activate_plugin( 'wp-graphql/wp-graphql.php' );
+
+  add_filter('rest_url', function($url) {
+    return str_replace(home_url( '/wp' ), site_url(), $url);
+  });
+
+  switch_theme('headless');
+?>
