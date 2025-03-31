5ac93222c1515173625b9f68b7d35976876c7bf9
R. Ilham Sastronegoro <7355835+radenpioneer@users.noreply.github.com>
2025-03-31 17:56:37 +0000

    Add morgan middleware for logging requests

--- web/server.mjs
+++ web/server.mjs
@@ -7,8 +7,8 @@ import { handler as ssrHandler } from './dist/server/entry.mjs'
 
 const app = express()
 
+app.use(morgan('common'))
 app.use('/', express.static('./dist/client'))
 app.use(ssrHandler)
-app.use(morgan('common'))
 
 app.listen(4321, '0.0.0.0')
