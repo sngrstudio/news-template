9d13ad64023897cf0f6b19fe885657e1fa8b4cd3
R. Ilham Sastronegoro <7355835+radenpioneer@users.noreply.github.com>
2025-06-24 10:13:52 +0000

    feat: refactor pages loader and update dynamic page rendering

--- web/src/content/pages.ts
+++ web/src/content/pages.ts
@@ -1,28 +1,45 @@
 import { defineCollection } from 'astro:content'
+import type { Loader } from 'astro/loaders'
 import { getPages } from '~/graphql/pages'
 import { z } from 'astro:schema'
 
-const schema = z.object({
-  title: z.string(),
-  uri: z.string(),
-  featuredImage: z.string().optional(),
-  content: z.string().optional()
-})
+export const pagesLoader = (): Loader => {
+  return {
+    name: 'pages-loader',
+    load: async (ctx) => {
+      ctx.logger.info('Loading static pages...')
+      const pages = await getPages()
+      ctx.store.clear()
+
+      for (const page of pages) {
+        const data = await ctx.parseData({
+          id: page.uri!,
+          data: {
+            ...page,
+            featuredImage: page.featuredImage?.node.sourceUrl ?? undefined
+          }
+        })
+        const digest = ctx.generateDigest(data)
+        ctx.store.set({
+          id: page.uri!,
+          data,
+          rendered: {
+            html: data.content ?? ''
+          },
+          digest
+        })
+      }
+    },
+    schema: z.object({
+      title: z.string(),
+      uri: z.string(),
+      featuredImage: z.string().optional()
+    })
+  }
+}
 
 const pages = defineCollection({
-  loader: async () => {
-    const pages = await getPages()
-    return pages.map((page) => {
-      return {
-        id: page.uri!,
-        title: page.title!,
-        uri: page.uri!,
-        featuredImage: page.featuredImage?.node.sourceUrl,
-        content: page.content || ''
-      }
-    })
-  },
-  schema
+  loader: pagesLoader()
 })
 
 export default pages
